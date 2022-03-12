f8122ce088dd7688b7d832c0fe9834e55739e4e7
anthony_lee2 <anthonyleetti@gmail.com>
2022-03-12 17:01:11 +0800

    Chain route handlers using Express

--- app.js
+++ app.js
@@ -22,46 +22,46 @@ const Article = mongoose.model("Article", articleSchema);
 
 // RESTful API demonstartion
 
-// Read
-app.get("/articles", (req, res) => {
-    Article.find({}, (err, foundArticles) => {
-        res.send(err || foundArticles);
-    });
-});
-
-// Create
-app.post("/articles", (req, res) => {
-    const title = req.body.title;
-    const content = req.body.content;
+// chained route handlers using Express
+app.route("/articles")
+    // Read
+    .get((req, res) => {
+        Article.find({}, (err, foundArticles) => {
+            res.send(err || foundArticles);
+        });
+    })
+    // Create
+    .post((req, res) => {
+        const title = req.body.title;
+        const content = req.body.content;
+        
+        // Method 1
+        const newArticle = new Article({
+            title: title,
+            content: content,
+        });
     
-    // Method 1
-    const newArticle = new Article({
-        title: title,
-        content: content,
+        newArticle.save(err => {
+            res.send(err || "Successfully added a new article!");
+        });
+    
+        // Method 2
+        // Article.insertMany([{title: title, content: content}], err => {
+        //     res.send(err || "Successfully added a new article!");
+        // });
+    
+        // Method 3
+        // Article.create({title: title, content: content}, (err, article) => {
+        //     res.send(err || "Successfully added a new article!");
+        // });
+    
+    })
+    // Delete
+    .delete((req, res) => {
+        Article.deleteMany({}, err => {
+            res.send(err || `Successfully deleted all articles!`);
+        });
     });
-
-    newArticle.save(err => {
-        res.send(err || "Successfully added a new article!");
-    });
-
-    // Method 2
-    // Article.insertMany([{title: title, content: content}], err => {
-    //     res.send(err || "Successfully added a new article!");
-    // });
-
-    // Method 3
-    // Article.create({title: title, content: content}, (err, article) => {
-    //     res.send(err || "Successfully added a new article!");
-    // });
-
-});
-
-// Delete
-app.delete("/articles", (req, res) => {
-    Article.deleteMany({}, err => {
-        res.send(err || `Successfully deleted all articles!`);
-    });
-});
 
 app.listen(3000, () => {
     console.log("Server is opening on port 3000.");
